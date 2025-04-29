1bee6f5a1fbdccfbe892c1f3f97ab143a0f74a4f
ActuallyNaf <126985757+ActuallyNaf@users.noreply.github.com>
2025-04-30 00:50:31 +0300

    Initial commit

---
+++ JokerTweaks.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Joker Tweaks
+--- MOD_ID: jokertweaks
+--- MOD_AUTHOR: [Naf]
+--- MOD_DESCRIPTION: Improves/changes some of the jokers (based on my personal preference). Currently changed: Obelisk, Ride the Bus, Square Joker
+
+--- PRIORITY: -5
+--- DISPLAY_NAME: Joker Tweaks
+--- BADGE_COLOUR: d18017
+
+----------------------------------------------
+------------ MOD CODE ------------------------
+
+----------------------------------------------
+------------ END MOD CODE --------------------
---
+++ localization/en-us.lua
@@ -0,0 +1,23 @@
+return {
+	descriptions = {
+		Joker = {
+			j_ride_the_bus = {
+				text={
+                    "This Joker gains {C:mult}+#1#{} Mult",
+                    "if played hand doesn't",
+                    "score a {C:attention}face{} card",
+                    "{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult)",
+                }
+			},
+			
+			j_obelisk = {
+				text={
+                    "This Joker gains {X:mult,C:white} X#1# {} Mult",
+                    "if played hand is not",
+                    "your most played {C:attention}poker hand",
+                    "{C:inactive}(Currently {X:mult,C:white} X#2# {C:inactive} Mult)",
+                }
+			}
+		}
+	}
+}
